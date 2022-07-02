b6190308f7c7f57297ce571198ba0a5f940e4ed5
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2022-07-02 14:47:26 -0500

    Add test for Post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'it is not valid without title' do
+    post = Post.new
+
+    assert_not post.valid?
+    assert_equal ["can't be blank"], post.errors.messages[:title]
+  end
+
+  test 'it is not valid without content' do
+    post = Post.new
+
+    #binding.irb
+    assert_not post.valid?
+    assert_equal ["can't be blank"], post.errors.messages[:content]
+  end
+
+  test 'it is valid with title and content' do
+    post = Post.new(title: 'My post', content: 'My new blog post')
+
+    assert post.valid?
+  end
 end
